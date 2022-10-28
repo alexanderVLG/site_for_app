08a2c8e5f056c1b5f0ff1612a73a5c8266d0673b
alexanderVLG <dominorog323@gmail.com>
2022-10-28 20:02:10 +0500

    first commit

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import '../styles/Button.css';
+
+class Button extends Component {
+  
+  render () {
+    return (
+      <button className="header__button">Установить</button>
+    );
+  }
+}
+
+export default Button;
---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import Button from '../components/Button.js';
+import '../styles/Navbar.css';
+import Logo from'../img/Logo.png';
+
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      visible : false
+    }
+
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu () {
+    this.setState({
+      visible: !this.state.visible
+    })
+  }
+  render() {
+    return (
+      <header>
+        <a href="index.html">
+          <img src={Logo} alt="Логотип"/>
+        </a>
+        <div className='burger__menu'>
+          <span></span>
+        </div>
+{/*--this nav shows up after max-width 950px--*/ }      
+      <nav className='desktop__nav'>
+        <ul>
+          <li><a href="#">Магазины</a></li>
+          <li><a href="#">Помощь и Поддержка</a></li>
+          <li><a href="#"><Button/></a></li>
+        </ul>
+      </nav>
+      </header>
+      
+    );
+  }
+}
+
+export default Navbar;
